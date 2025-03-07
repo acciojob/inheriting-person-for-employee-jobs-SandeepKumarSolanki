34e787b7896831fb2ad6ce6c4b07fcb91b24512b
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-03-07 23:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// complete this js code
-class Person{
-	constructor(name , age){
-		this.name = name;
-		this.age = age;
-	}
+// Person Class Definition
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	}
-}
-class Employee extends Person{
-	constructor(name , age , jobTitle){
-		super(name , age);
-		this.jobTitle = jobTitle;
-	}
-
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-	}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
+// Employee Class Definition that Inherits from Person
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age);  // Call the parent class constructor
+    this.jobTitle = jobTitle;
+  }
 
-function Person(name, age) {}
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
 
-function Employee(name, age, jobTitle) {}
-
-// Do not change code below this line
+// Do not change the code below this line
 window.Person = Person;
 window.Employee = Employee;
